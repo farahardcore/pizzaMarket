40699bb5c06d84e7e74c2cacedc49f608fecbe45
Farid <farahardcore161@gmail.com>
2020-09-07 22:32:58 +0300

    create project

---
+++ src/Components/Menu/Menu.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Menu() {
+    return (
+        <div className="menu">
+            Menu
+        </div>
+    );
+}
+
+export default Menu;
---
+++ src/Components/Slider/Slider.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Slider() {
+    return (
+        <div className="slider">
+            Slider
+        </div>
+    );
+}
+export default Slider;
